8a24934f0bc65d282320c565cea22e6cdba5eecd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-27 12:59:16 -0700

    Finished happypet

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -11,8 +11,19 @@ public class HappyPet {
 int Num = JOptionPane.showOptionDialog(null, "CHOOSE YOU PET", "CHOOSE YOU PET", 0, JOptionPane.INFORMATION_MESSAGE, null,
 		new String[] { "Cat", "Dog", "Lizard"}, null);
 
-String Pet
+String Pet = "";
+if(Num==0) {
+	Pet = "Cat";
+}
+else if(Num==1) {
+	Pet = "Dog";
+}
+else if(Num==2) {
+	Pet = "Lizard";
+}
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		for (int i = 0; i < 20; i++) {
+			
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
@@ -28,38 +39,62 @@ else if(task==1) {
 else if(task==2) {
 	Toy(Pet);
 }
+		
 			// 5. Use user input to call the appropriate method created in step 4.
 
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+		if(hapinesslevel>100) {
+			JOptionPane.showMessageDialog(null, "Congratulations; Your pet officially loves you!");
+		break;
+		}
+		}
 	}
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
 	static void Song (String param) {
-		if(param.equalsIgnoreCase("Lizard")) {
+		if(param.equals("Lizard")) {
 		JOptionPane.showMessageDialog(null, "Lizards can't hear sounds, only feel vibrations.");
-	hapinesslevel=(+10);
+	    hapinesslevel=hapinesslevel+10;
 		}
-		else if(param.equalsIgnoreCase("Dog")) {
+		else if(param.equals("Dog")) {
 			JOptionPane.showMessageDialog(null, "Dogs always look happy when you talk to them, I doubt singing is any different!");
-		hapinesslevel=(+20);
+		hapinesslevel=hapinesslevel+20;
 		}
-		else if(param.equalsIgnoreCase("Cat")) {
+		else if(param.equals("Cat")) {
 			JOptionPane.showMessageDialog(null, "Cats have VERY sensitive hearing, so keep your voice down.");
-		hapinesslevel=(+15);
+		hapinesslevel=hapinesslevel+10;
 		}
 	}
 
 	static void Feed(String param) {
-		
-		JOptionPane.showMessageDialog(null, "Oh yeah I guess that's necessary. Your "+param+" is now full.");
-		hapinesslevel=(+15);
+		if(param.equals("Lizard")) {
+		JOptionPane.showMessageDialog(null, "Just put in some crickets and watch the show!");
+		hapinesslevel=hapinesslevel+15;
+		}
+		else if(param.equals("Dog")) {
+			JOptionPane.showMessageDialog(null, "Dogs eat a anything from my experience, including bacon, hamburgers, cheese...");
+		hapinesslevel=hapinesslevel+25;
+		}
+		else if(param.equals("Cat")) {
+			JOptionPane.showMessageDialog(null, "I don't actually know what you feed a cat... Does kitty kibble exist or something??");
+		hapinesslevel=hapinesslevel+15;
+		}
 	}
-	
 	static void Toy(String param) {
-		JOptionPane.showMessageDialog(null, "");
+		if(param.equals("Lizard")) {
+			JOptionPane.showMessageDialog(null, "What lizard do you have that plays with toys?");
+		hapinesslevel=hapinesslevel+5;
+		}
+		else if(param.equals("Dog")) {
+		JOptionPane.showMessageDialog(null, "FETCH! If you couldn't tell, dogs are meant to be the easiest in this game");
+	    hapinesslevel=hapinesslevel+20;
+	    }
+	else if(param.equals("Cat")) {
+		JOptionPane.showMessageDialog(null, "And here we observe the ferocious housecat as it stalks it's prey... a stuffed mouse");
+		hapinesslevel=hapinesslevel+20;
+	}
 	}
 }
