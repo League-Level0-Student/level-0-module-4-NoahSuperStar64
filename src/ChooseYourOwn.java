060ac877d2c7ced066d1447a71d1965f1f313741
noaht <noaht@172.31.48.21>
2019-04-17 12:58:09 -0700

    Worked on chooseyourown AGAIN

--- src/ChooseYourOwn.java
+++ src/ChooseYourOwn.java
@@ -4,35 +4,41 @@ public class ChooseYourOwn {
 public static void main(String[] args) {
 	
 	
-	JOptionPane.showMessageDialog(null, "This is the world of Hocolatia. Previously a peaceful land,\n the tranquility has been broken by the rising forces of the evil Queen Umbra, the world-consuming Termina Catastrophe, and the mecha-beast Moruega.\n Only you and two others can stop them.");
-
-	String Hero = JOptionPane.showInputDialog("Name yourself, a valiant archer gifted with magical wings");
-	String MagicUser = JOptionPane.showInputDialog("Name your friend, a brilliant spellcaster well versed in the ancient art of magic.");
-	String Monster = JOptionPane.showInputDialog("Finally, name the massive beast that you'll meet on your journey.");
+	JOptionPane.showMessageDialog(null, "This is the world of Hocolatia. Previously a peaceful land,\n the tranquility has been broken by the rising forces of the evil Queen Umbra, the world-consuming Termina Catastrophe, and the mecha-beast Moruega.\n Only you can stop them, blah blah, you've heard this backstory before.");
 	
-int Story1 = JOptionPane.showOptionDialog(null, "The capital city Hocolan is under siege by Queen Umbra's dark knights.\n You manage to escape along with "+MagicUser+". You have a few options here, so choose wisely.", "Capital falling", 0, JOptionPane.INFORMATION_MESSAGE, null,
-			new String[] { "Charge back in and battle the knights", "Retreat for now", "Try and shoot the knights from a distance"}, null);
+int Story1 = JOptionPane.showOptionDialog(null, "The capital city Hocolan is under siege by Queen Umbra's dark knights.\n You manage to escape the city walls. You have a few options here, so choose wisely.", "Capital falling", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Charge back in and swordfight the knights", "Retreat for now", "Try and shoot the knights from a distance with your bow"}, null);
 	if(Story1==0) {
-		JOptionPane.showMessageDialog(null, "That barely worked...\n But you managed to take back hocolan with some brute force.");
+		JOptionPane.showMessageDialog(null, "That barely worked...\n But due to the knights utter stupidity, you managed to take back hocolan with some brute force.");
+	Hocolan(Story1);
+	}
 	
+	else if(Story1==2) {
+	JOptionPane.showMessageDialog(null, "After some careful sharpshooting, Hocolan is clear of dark knights");
+	Hocolan(Story1);
 	}
 	
 	else if(Story1==1) {
-		JOptionPane.showMessageDialog(null, "You decide that a tactical retreat is the best option, and you start traveling away from hocolan.");
+		
+	JOptionPane.showOptionDialog(null, "You decide that a tactical retreat is the best option.\n Where do you end up?", "Bravely running away", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Jellyfish Forest", "Ice Kingdom", "PlaceholderTitle"}, null);
 	}
+
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		
+}
+
+static void Hocolan (int Stupidity) {
 	
 }
+
+
+
+
+
+
+
+
 }
