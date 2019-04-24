7bb0b764522c01a8fba4160d8c4919ddc9418461
noaht <noaht@172.31.48.40>
2019-04-24 13:01:15 -0700

    Tried and failed to make bohemian rhapsody

---
+++ src/Bohemianrhapsody.java
@@ -0,0 +1,38 @@
+
+public class Bohemianrhapsody {
+public static void main(String[] args) {
+	try
+	{
+		Assert(life.real);
+		Assert(life.fantasy);
+	}
+	catch (LandSlideException ex)
+}
+
+	#region Reality while(true)
+{
+	character.Eyes.forEach(eye =>
+	eye.Open().Orient(Direction.sky).see()
+	);
+	this.Wealth = 1/Integer.MAX_VALUE;
+	this.Sex = Sex.Male;
+	
+	this.Sympathies.Clear();
+	
+	if(this.comeDifficulty == Difficulty.Easy &&
+	   this.goDifficulty == Difficulty.Easy &&
+	   this.High < 0.1 &&
+	   this.Low < 0.1)
+	{
+		switch (wind.Direction)
+	}
+	case Direction.North
+	case Direction.East
+	case Direction.South
+	case Direction.West
+	default:
+		this.Matter = false;
+		piano.play();
+		break;
+}
+}#endregion}}}}
--- src/ChooseYourOwn.java
+++ src/ChooseYourOwn.java
@@ -1,44 +1,44 @@
 import javax.swing.JOptionPane;
 
 public class ChooseYourOwn {
-public static void main(String[] args) {
-	
-	
-	JOptionPane.showMessageDialog(null, "This is the world of Hocolatia. Previously a peaceful land,\n the tranquility has been broken by the rising forces of the evil Queen Umbra, the world-consuming Termina Catastrophe, and the mecha-beast Moruega.\n Only you can stop them, blah blah, you've heard this backstory before.");
-	
-int Story1 = JOptionPane.showOptionDialog(null, "The capital city Hocolan is under siege by Queen Umbra's dark knights.\n You manage to escape the city walls. You have a few options here, so choose wisely.", "Capital falling", 0, JOptionPane.INFORMATION_MESSAGE, null,
-			new String[] { "Charge back in and swordfight the knights", "Retreat for now", "Try and shoot the knights from a distance with your bow"}, null);
-	if(Story1==0) {
-		JOptionPane.showMessageDialog(null, "That barely worked...\n But due to the knights utter stupidity, you managed to take back hocolan with some brute force.");
-	Hocolan(Story1);
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null,
+				"This is the world of Hocolatia. Previously a peaceful land,\n the tranquility has been broken by the rising forces of the evil Queen Umbra, the world-consuming Termina Catastrophe, and the mecha-beast Moruega.\n Only you can stop them, blah blah, you've heard this backstory before.");
+
+		int Story1 = JOptionPane.showOptionDialog(null,
+				"The capital city Hocolan is under siege by Queen Umbra's dark knights.\n You manage to escape the city walls. You have a few options here, so choose wisely.",
+				"Capital falling", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Charge back in and swordfight the knights", "Retreat for now",
+						"Try and shoot the knights from a distance with your bow" },
+				null);
+		if (Story1 == 0) {
+			JOptionPane.showMessageDialog(null,
+					"That barely worked...\n But due to the knights utter stupidity, you managed to take back hocolan with some brute force.");
+			Hocolan(Story1);
+		}
+
+		else if (Story1 == 2) {
+			JOptionPane.showMessageDialog(null, "After some careful sharpshooting, Hocolan is clear of dark knights");
+			Hocolan(Story1);
+		}
+
+		else if (Story1 == 1) {
+
+			int Story2 = JOptionPane.showOptionDialog(null,
+					"You decide that a tactical retreat is the best option.\n Where do you end up?",
+					"Bravely running away", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Jellyfish Forest", "Ice Kingdom", "PlaceholderTitle" }, null);
+                    if(Story2==0) {
+                    	JOptionPane.showMessageDialog(null, "You arrive at the Jellyfish Forest, a beautiful place of tranquility and ferocity.\nThough the only thing close to a jellyfish in it is\nNomurox, a powerful alien-like protector of the forest.");
+                    
+                    }
+		}
+
 	}
-	
-	else if(Story1==2) {
-	JOptionPane.showMessageDialog(null, "After some careful sharpshooting, Hocolan is clear of dark knights");
-	Hocolan(Story1);
+
+	static void Hocolan(int Stupidity) {
+
 	}
-	
-	else if(Story1==1) {
-		
-	JOptionPane.showOptionDialog(null, "You decide that a tactical retreat is the best option.\n Where do you end up?", "Bravely running away", 0, JOptionPane.INFORMATION_MESSAGE, null,
-			new String[] { "Jellyfish Forest", "Ice Kingdom", "PlaceholderTitle"}, null);
-	}
-
-	
-	
-	
-		
-}
-
-static void Hocolan (int Stupidity) {
-	
-}
-
-
-
-
-
-
-
 
 }
