6792a2b0628e8faf076e6556dd3cb7800e4a7f2b
noaht <noaht@172.31.48.51>
2019-05-01 12:59:10 -0700

    Worked on module4

--- src/ChooseYourOwn.java
+++ src/ChooseYourOwn.java
@@ -7,7 +7,7 @@ public class ChooseYourOwn {
 				"This is the world of Hocolatia. Previously a peaceful land,\n the tranquility has been broken by the rising forces of the evil Queen Umbra, the world-consuming Termina Catastrophe, and the mecha-beast Moruega.\n Only you can stop them, blah blah, you've heard this backstory before.");
 
 		int Story1 = JOptionPane.showOptionDialog(null,
-				"The capital city Hocolan is under siege by Queen Umbra's dark knights.\n You manage to escape the city walls. You have a few options here, so choose wisely.",
+				"The capital city Hocolan is under siege by Queen Umbra's dark knights.\n You manage to escape the city walls with a cutlass and a bow to defend yourself. You have a few options here, so choose wisely.",
 				"Capital falling", 0, JOptionPane.INFORMATION_MESSAGE, null,
 				new String[] { "Charge back in and swordfight the knights", "Retreat for now",
 						"Try and shoot the knights from a distance with your bow" },
@@ -29,16 +29,22 @@ public class ChooseYourOwn {
 					"You decide that a tactical retreat is the best option.\n Where do you end up?",
 					"Bravely running away", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "Jellyfish Forest", "Ice Kingdom", "PlaceholderTitle" }, null);
-                    if(Story2==0) {
-                    	JOptionPane.showMessageDialog(null, "You arrive at the Jellyfish Forest, a beautiful place of tranquility and ferocity.\nThough the only thing close to a jellyfish in it is\nNomurox, a powerful alien-like protector of the forest.");
-                    
-                    }
+			if (Story2 == 0) {
+				JOptionPane.showMessageDialog(null,
+						"You arrive at the Jellyfish Forest, a beautiful place of tranquility and ferocity.\nThough the only thing close to a jellyfish in it is\nNomurox, a powerful alien-like protector of the forest.");
+
+			}
 		}
 
 	}
 
 	static void Hocolan(int Stupidity) {
-
+int Story3 = JOptionPane.showOptionDialog(null, "Now that the dark knights have been driven out of Hocolan the people have started to return to their houses.", "The fallen Capital", 0, JOptionPane.INFORMATION_MESSAGE, null, 
+		new String[] { "Stay and restore order", "Lead an all out assault on Umbra's castle", "Fortnite dance to assert dominance" }, null);
+	if(Story3==0) {
+		JOptionPane.showMessageDialog(null, "Hocolan is rebuilt, but the presence of Queen Umbra remains a passive threat on the Hocolation's daily lives until the end of time. THE END");
+	}
+	
 	}
 
 }
--- src/extra/FruitQuiz.java
+++ src/extra/FruitQuiz.java
@@ -14,7 +14,7 @@ import javax.swing.JLabel;
 public class FruitQuiz extends KeyAdapter {
 
 	void makeQuestions() {
-		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
+		question1 = new JLabel("<html>Which is not a real kaiju? fruit is boring so I changed it <br> A: Varan <br> B: Tyranitar <br> C: Baragon</html>");
 		// 11. Make another question called "question2".  Use question1 above as a guide.
 	}
 
@@ -22,9 +22,11 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+		int A = 65;
+		int B = 66;
+		int C = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
@@ -34,12 +36,13 @@ public class FruitQuiz extends KeyAdapter {
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
+			if(keyCode==B) {	
 			
 				// 4. Call the correct() method
-				
+				correct();
 				// 7. Use the nextQuestion() method to go to question2
-			
-			
+			nextQuestion();
+			}
 			// 8. else (if they touched something else)
 				
 				// 9. Call the incorrect() method
@@ -53,13 +56,13 @@ public class FruitQuiz extends KeyAdapter {
 		// the 'extra' package. It must be a .wav file. 
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+playSound("correct.wav");
 
 	}
 
 	private void incorrect() {
 		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
-
+playSound();
 	}
 
 	private void nextQuestion(JLabel newQuestion) {
